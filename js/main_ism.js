a3747d43f6801ef91fe9fd7e9ad103ed9ccab95d
김준걸 <dumicode@kakao.com>
2024-09-09 21:56:50 +0900

    산업보안관리사 레이아웃 완료

---
+++ js/main_ism.js
@@ -0,0 +1,130 @@
+$(function () {
+
+  //main news tab
+  var $newsTabLink = $(".ma_news_tab a");
+  $newsTabLink.on("click focus", function (e) {
+    e.preventDefault();
+    $(this).parents(".ma_news_tab").find("> li").removeClass("active");
+    $(this).parents(".ma_news_tab > li").addClass("active");
+  });
+
+  //main alarm
+  var maAlarm = new Swiper(".ma_alarm .swiper-container", {
+    spaceBetween: 16,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    observer: true,
+    observeParents: true,
+    pagination: {
+      el: ".ma_alarm .swiper-pagination",
+      type: "custom",
+      renderCustom: function (swiper, current, total) {
+        return '<span class="current">' + current + "</span>" + '<span class="slash">/</span>' + '<span class="total">' + total + "</span>";
+      },
+    },
+    navigation: {
+      nextEl: ".ma_alarm .swiper-button-next",
+      prevEl: ".ma_alarm .swiper-button-prev",
+    },
+    on: {
+      init: function (swiper) {
+        $(".ma_alarm .swiper-slide").attr("aria-hidden", true);
+        $(".ma_alarm .swiper-slide-active").removeAttr("aria-hidden");
+        $(".ma_alarm .swiper-slide a, .ma_alarm .swiper-slide .btn").attr("tabindex", "-1");
+        $(".ma_alarm .swiper-slide-active a, .ma_alarm .swiper-slide-active .btn").attr("tabindex", "0");
+
+        //자동play 켜고닫기
+        $(".ma_alarm .swiper_ctrl .btn_stop_play").click(function () {
+          if ($(this).hasClass("stop")) {
+            maAlarm.autoplay.start();
+          } else {
+            maAlarm.autoplay.stop();
+          }
+        });
+      },
+      slideChangeTransitionStart: function () {
+        $(".ma_alarm .swiper-slide").attr("aria-hidden", true);
+        $(".ma_alarm .swiper-slide-active").removeAttr("aria-hidden");
+        $(".ma_alarm .swiper-slide a, .ma_alarm .swiper-slide .btn").attr("tabindex", "-1");
+        $(".ma_alarm .swiper-slide-active a, .ma_alarm .swiper-slide-active .btn").attr("tabindex", "0");
+      },
+      autoplayStart: function () {
+        $(".ma_alarm .swiper_ctrl .btn_stop_play").removeClass("stop");
+      },
+      autoplayStop: function () {
+        $(".ma_alarm .swiper_ctrl .btn_stop_play").addClass("stop");
+      },
+    },
+  });
+
+  //main EduEvt
+  var maEduEvt = new Swiper(".eduevt_swiper", {
+    slidesPerView: 2,
+    spaceBetween: 24,
+    breakpoints: {
+      767: {
+        slidesPerView: 1,
+      },
+    },
+    observer: true,
+    observeParents: true,
+  });
+
+  //간행물
+  $('.ma_publications .intab a').on('click', function(){
+    $('.ma_publications .intab a').removeAttr('aria-selected');
+    $(this).attr('aria-selected','true');
+
+    $('.ma_publications .intabcon').hide().eq($('.ma_publications .intab a').index(this)).show();
+    
+    return false;
+  })
+  var maPublications1 = new Swiper(".pc_swp01 .swiper-container", {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    breakpoints: {
+      767: {
+        slidesPerView: 1,
+      },
+    },
+    observer: true,
+    observeParents: true,
+    pagination: {
+      el: ".pc_swp01 .swiper-pagination",
+      type: "custom",
+      renderCustom: function (swiper, current, total) {
+        return '<span class="current">' + current + "</span>" + '<span class="slash">/</span>' + '<span class="total">' + total + "</span>";
+      },
+    },
+    navigation: {
+      nextEl: ".pc_swp01 .swiper-button-next",
+      prevEl: ".pc_swp01 .swiper-button-prev",
+    },
+  });
+
+  var maPublications2 = new Swiper(".pc_swp02 .swiper-container", {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    breakpoints: {
+      767: {
+        slidesPerView: 1,
+      },
+    },
+    observer: true,
+    observeParents: true,
+    pagination: {
+      el: ".pc_swp02 .swiper-pagination",
+      type: "custom",
+      renderCustom: function (swiper, current, total) {
+        return '<span class="current">' + current + "</span>" + '<span class="slash">/</span>' + '<span class="total">' + total + "</span>";
+      },
+    },
+    navigation: {
+      nextEl: ".pc_swp02 .swiper-button-next",
+      prevEl: ".pc_swp02 .swiper-button-prev",
+    },
+  });
+});
